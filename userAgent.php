e88276d663733e7d8d6a7cff3914b47def337539
phpfail <wowduder@gmail.com>
2017-02-11 09:39:29 -0700

    Update userAgent.php

--- userAgent.php
+++ userAgent.php
@@ -1,30 +1,164 @@
+<?php
+/** **********************************************************************************
+ * Generates a completely random user agent based upon operating system/version and browser/version.
+ *    Supported OS:       Windows, Linux, Mac OS, iOS, Android, and Windows Phone
+ *    Supported Browsers: Firefox, Chrome, Opera, Safari
+ */
 class userAgent {
    /**
     * Windows Operating System list with dynamic versioning
     */
-   public $windows_os = [ 'Windows; U; Windows NT 6.:number0-3:;', 'Windows NT 6.:number0-3:; Win64; x64', 'Windows; U; Windows NT 6.:number0-3:;', 'Windows; Windows NT 6.:number0-3:; WOW64', 'Windows; Windows NT 6.:number0-3:; WOW64', 'Windows NT 6.:number0-3:; Win64; WOW64', 'Windows; Windows NT 6.:number0-3:', 'Windows NT 6.:number0-3:;', 'Windows; Windows NT 10.0', 'Windows NT 10.0; x64;', 'Windows NT 10.0; Win64; x64;', 'Windows NT 10.0; WOW64', 'Windows; Windows NT 10.0;', 'Windows; Windows NT 10.0; x64' ];
+   public $windows_os = [
+     '[Windows; |Windows; U; |]Windows NT 6.:number0-3:;[ Win64; x64;| WOW64;| x64;|]',
+     '[Windows; |Windows; U; |]Windows NT 10.0;[ Win64; x64;| WOW64;| x64;|]',
+   ];
    /**
     * Linux Operating Systems [limited]
     */
-   public $linux_os = [ 'Linux x86_64', 'Linux i686', ];
+   public $linux_os = [
+     'Linux x86_64',
+     'Linux i686'
+   ];
    /**
     * Mac Operating System (OS X) with dynamic versioning
     */
-   public $mac_os = [ 'Macintosh; U; Intel Mac OS X 8_:number0-9:_:number0-9:;', 'Macintosh; Intel Mac OS X 8_:number0-12:_:number0-9:', 'Macintosh; U; Intel Mac OS X 9_:number0-9:_:number0-9:;', 'Macintosh; Intel Mac OS X 9_:number0-12:_:number0-9', 'Macintosh; U; Intel Mac OS X 10_:number0-9:_:number0-9;', 'Macintosh; Intel Mac OS X 10_:number0-12:_:number0-9:' ];
+   public $mac_os = [
+     'Macintosh; {U; |]Intel Mac OS X :number7-9:_:number0-9:_:number0-9:;',
+     'Macintosh; [U; |]Intel Mac OS X 10_:number0-12:_:number0-9:'
+   ];//<<start_formatting
+   public $androidVersions = ['4.3.1', '4.4', '4.4.1', '4.4.4', '5.0', '5.0.1', '5.0.2', '5.1', '5.1.1', '6.0', '6.0.1', '7.0', '7.1','7.1.1'];//<<end_formatting
+   public $androidVersion;
+   public $androidDevices = [
+     '4.3' => [
+       'GT-I9:number2-5:00 Build/JDQ39',
+       'Nokia 3:number1-3:[10|15] Build/IMM76D',
+       '[SAMSUNG |]SM-G3:number1-5:0[R5|I|V|A|T|S] Build/JLS36C',
+       'Ascend G3:number0-3:0 Build/JLS36I',
+       '[SAMSUNG |]SM-G3:number3-6::number1-8::number0-9:[V|A|T|S|I|R5] Build/JLS36C',
+       'HUAWEI G6-L:number10-11: Build/HuaweiG6-L:number10-11:',
+       '[SAMSUNG |]SM-[G|N]:number7-9:1:number0-8:[S|A|V|T] Build/[JLS36C|JSS15J]',
+       '[SAMSUNG |]SGH-N0:number6-9:5[T|V|A|S] Build/JSS15J',
+       'Samsung Galaxy S[4|IV] Mega GT-I:number89-95:00 Build/JDQ39',
+       'SAMSUNG SM-T:number24-28:5[s|a|t|v] Build/[JLS36C|JSS15J]',
+       'HP :number63-73:5 Notebook PC Build/[JLS36C|JSS15J]',
+       'HP Compaq 2:number1-3:10b Build/[JLS36C|JSS15J]',
+       'HTC One 801[s|e] Build/[JLS36C|JSS15J]',
+       'HTC One max Build/[JLS36C|JSS15J]',
+       'HTC Xplorer A:number28-34:0[e|s] Build/GRJ90',
+     ],
+     '4.4' => [
+       'XT10:number5-8:0 Build/SU6-7.3',
+       'XT10:number12-52: Build/[KXB20.9|KXC21.5]',
+       'Nokia :number30-34:10 Build/IMM76D',
+       'E:number:20-23::number0-3::number0-4: Build/24.0.[A|B].1.34',
+       '[SAMSUNG |]SM-E500[F|L] Build/KTU84P',
+       'LG Optimus G Build/KRT16M',
+       'LG-E98:number7-9: Build/KOT49I',
+       'Elephone P:number2-6:000 Build/KTU84P',
+       'IQ450:number0-4: Quad Build/KOT49H',
+       'LG-F:number2-5:00[K|S|L] Build/KOT49[I|H]',
+       'LG-V:number3-7::number0-1:0 Build/KOT49I',
+       '[SAMSUNG |]SM-J:number1-2::number0-1:0[G|F] Build/KTU84P',
+       '[SAMSUNG |]SM-N80:number0-1:0 Build/[KVT49L|JZO54K]',
+       '[SAMSUNG |]SM-N900:number5-8: Build/KOT49H',
+       '[SAMSUNG-|]SGH-I337[|M] Build/[JSS15J|KOT49H]',
+       '[SAMSUNG |]SM-G900[W8|9D|FD|H|V|FG|A|T] Build/KOT49H',
+       '[SAMSUNG |]SM-T5:number30-35: Build/[KOT49H|KTU84P]',
+       '[Google |]Nexus :number5-7: Build/KOT49H',
+       'LG-H2:number0-2:0 Build/KOT49[I|H]',
+       'HTC One[_M8|_M9|0P6B|801e|809d|0P8B2|mini 2|S][ dual sim|] Build/[KOT49H|KTU84L]',
+       '[SAMSUNG |]GT-I9:number3-5:0:number0-6:[V|I|T|N] Build/KOT49H',
+       'Lenovo P7:number7-8::number1-6: Build/[Lenovo|JRO03C]',
+       'LG-D95:number1-8: Build/KOT49[I|H]',
+       'LG-D:number1-8::number0-8:0 Build/KOT49[I|H]',
+       'Nexus5 V:number6-7:.1 Build/KOT49H',
+       'Nexus[_|] :number4-10: Build/[KOT49H|KTU84P]',
+       'Nexus[_S_| S ][4G |]Build/GRJ22',
+       '[HM NOTE|NOTE-III|NOTE2 1LTE[TD|W|T]',
+       'ALCATEL ONE[| ]TOUCH 70:number2-4::number0-9:[X|D|E|A] Build/KOT49H',
+       'MOTOROLA [MOTOG|MSM8960|RAZR] Build/KVT49L'
+     ],
+     '5.0' => [  
+       'Nokia :number10-11:00 [wifi|4G|LTE] Build/GRK39F',
+       'HTC 80:number1-2[s|w|e|t] Build/[LRX22G|JSS15J]',
+       'Lenovo A7000-a Build/LRX21M;',
+       'HTC Butterfly S [901|919][s|d|] Build/LRX22G',
+       'HTC [M8|M9|M8 Pro Build/LRX22G',
+       'LG-D3:number25-37: Build/LRX22G',
+       'LG-D72:number0-9: Build/LRX22G',
+       '[SAMSUNG |]SM-G4:number0-9:0 Build/LRX22[G|C]',
+       '[|SAMSUNG ]SM-G9[00|25|20][FD|8|F|F-ORANGE|FG|FQ|H|I|L|M|S|T] Build/[LRX21T|KTU84F|KOT49H]',
+       '[SAMSUNG |]SM-A:number7-8:00[F|I|T|H|] Build/[LRX22G|LMY47X]',
+       '[SAMSUNG-|]SM-N91[0|5][A|V|F|G|FY] Build/LRX22C',
+       '[SAMSUNG |]SM-[T|P][350|550|555|355|805|800|710|810|815] Build/LRX22G',
+       'LG-D7:number0-2::number0-9: Build/LRX22G',
+       '[LG|SM]-[D|G]:number8-9::number0-5::number0-9:[|P|K|T|I|F|T1] Build/[LRX22G|KOT49I|KVT49L|LMY47X]'
+     ],
+     '5.1' => [
+       'Nexus :number5-9: Build/[LMY48B|LRX22C]',
+       '[|SAMSUNG ]SM-G9[28|25|20][X|FD|8|F|F-ORANGE|FG|FQ|H|I|L|M|S|T] Build/[LRX22G|LMY47X]',
+       '[|SAMSUNG ]SM-G9[35|350][X|FD|8|F|F-ORANGE|FG|FQ|H|I|L|M|S|T] Build/[MMB29M|LMY47X]',
+       '[MOTOROLA |][MOTO G|MOTO G XT1068|XT1021|MOTO E XT1021|MOTO XT1580|MOTO X FORCE XT1580|MOTO X PLAY XT1562|MOTO XT1562|MOTO XT1575|MOTO X PURE XT1575|MOTO XT1570 MOTO X STYLE] Build/[LXB22|LMY47Z|LPC23|LPK23|LPD23|LPH223]'
+     ],
+     '6.0' => [
+       '[SAMSUNG |]SM-[G|D][920|925|928|9350][V|F|I|L|M|S|8|I] Build/[MMB29K|MMB29V|MDB08I|MDB08L]',
+       'Nexus :number5-7:[P|X|] Build/[MMB29K|MMB29V|MDB08I|MDB08L]',
+       'HTC One[_| ][M9|M8|M8 Pro] Build/MRA58K',
+       'HTC One[_M8|_M9|0P6B|801e|809d|0P8B2|mini 2|S][ dual sim|] Build/MRA58K'
+     ],
+     '7.0' => [
+       'Pixel [XL|C] Build/[NRD90M|NME91E]',
+       'Nexus :number5-9:[X|P|] Build/[NPD90G|NME91E]',
+       '[SAMSUNG |]GT-I:number91-98:00 Build/KTU84P',
+       'Xperia [V |]Build/NDE63X',
+       'LG-H:number90-93:0 Build/NRD90[C|M]'
+     ],
+     '7.1' => [
+       'Pixel [XL|C] Build/[NRD90M|NME91E]',
+       'Nexus :number5-9:[X|P|] Build/[NPD90G|NME91E]',
+       '[SAMSUNG |]GT-I:number91-98:00 Build/KTU84P',
+       'Xperia [V |]Build/NDE63X',
+       'LG-H:number90-93:0 Build/NRD90[C|M]'
+     ]
+   ];
    /**
-    * List of popular mobile OS, some with model/build numbers, includes: Android, iPhone, Windows Phone
+    * List of mobile UA Formats
     */
-   public $mobile_os = [ 'Linux; Android 5.1.1; SM-G928X Build/LMY47X', 'Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 950', 'Linux; Android 6.0.1; SM-G920V Build/MMB29K', 'Linux; Android 6.0.1; Nexus 6P Build/MMB29P', 'Linux; Android 6.0.1; E6653 Build/32.2.A.0.253', 'Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K', 'Linux; Android 7.0; Pixel C Build/NRD90M; wv', 'Linux; Android 6.0.1; SGP771 Build/32.2.A.0.253; wv', 'Linux; Android 5.1.1; SHIELD Tablet Build/LMY48C', 'Linux; Android 4.4.3; KFTHWI Build/KTU84M', 'Linux; Android 5.0.2', 'Linux; Android 5.0.2; LG-V410/V41020c Build/LRX22G', 'Linux; Android 4.3;', 'Linux; Android 4.3.1;', 'Linux; Android 4.4;', 'Linux; Android 4.4.4;', 'Android;', 'Linux; Android 5.0;', 'Linux; Android 5.0.2;', 'Linux; Android 5.1;', 'Linux; Android 5.1.1;', 'Linux; Android 6.0;', 'Linux; Android 6.0.1;', 'Linux; Android 2.3.7;', 'Linux; Android 7.1;', 'Linux; Android 7.1.1;', 'iPhone; U; CPU iPhone OS 7_1_2 like Mac OS X;', 'iPhone; U; CPU iPhone OS 9_3_5 like Mac OS X;', 'iPhone; U; CPU iPhone OS 10_1_1 like Mac OS X;', 'iPhone; U; CPU iPhone OS 10_2_1 like Mac OS X;', 'iPad; CPU OS 7_0 like Mac OSX', 'iPad; U; CPU OS 7_1 like Mac OSX', 'iPad; CPU OS 9_3_2 like Mac OSX', 'iPad; CPU OS 10_1 like Mac OSX', 'iPad; CPU OS 10_2 like Mac OSX', 'iPod; U; CPU OS 7_0 like Mac OSX', 'iPod; U; CPU OS 9_3 like Mac OSX', 'iPod; U; CPU OS 10_2_1 like Mac OSX', ];
+   
+   public $mobile_os = [
+     'Linux; Android :androidVersion:; :androidDevice:',
+     //TODO: Add a $windowsDevices variable that does the same as androidDevice
+     //'Windows Phone 10.0; Android :androidVersion:; :windowsDevice:',
+     'Linux; U; Android :androidVersion:; :androidDevice:',
+     'Android; Android :androidVersion:; :androidDevice:',
+     'iPhone; CPU iPhone OS :number7-10:_:number0-12:_:number0-9:; like Mac OS X;',
+     'iPad; CPU iPad OS :number7-10:_:number0-12:_:number0-9: like Mac OS X;',
+     'iPod; CPU iPod OS :number7-10:_:number0-12:_:number0-9:; like Mac OS X;',
+   ];
    public $os;
    
    public function getOS() {
       $_os         = $this->os ?? array_merge($this->windows_os, $this->linux_os, $this->mac_os);
       $selected_os = rtrim($_os[random_int(0, count($_os) - 1)], ';');
+      
       if( strpos($selected_os, ':number') !== FALSE ) {
-         return self::processVersion($selected_os);
-      } else {
-         return $selected_os;
+         $selected_os = self::processRandomNumbers($selected_os);
       }
+      return $selected_os;
+   }
+   
+   public function getMobileOS() {
+      $selected_os = rtrim($this->mobile_os[random_int(0, count($this->mobile_os) - 1)], ';');
+      if( strpos($selected_os, ':androidVersion:') !== FALSE ) {
+         $selected_os = $this->processAndroidVersion($selected_os);
+      }
+      if( strpos($selected_os, ':androidDevice:') !== FALSE ) {
+         $selected_os = $this->addAndroidDevice($selected_os);
+      }
+      if( strpos($selected_os, ':number') !== FALSE ) {
+         $selected_os = self::processRandomNumbers($selected_os);
+      }
+      return $selected_os;
    }
    
    public function setOS( $os ) {
@@ -32,8 +166,28 @@ class userAgent {
       return $this;
    }
    
-   public static function processVersion( $selected_os ) {
-      return preg_replace_callback('/:number(\d{1})-(\d{1,2}):/i', function( $matches ) { return random_int($matches[1], $matches[2]); }, $selected_os);
+   public static function processRandomNumbers( $selected_os ) {
+      return preg_replace_callback('/:number(\d+)-(\d+):/i', function( $matches ) { return random_int($matches[1], $matches[2]); }, $selected_os);
+   }
+   
+   public function processSpinSyntax( $selected_os ) {
+      return preg_replace_callback('/\[([a-z0-9\-\s\|]*?)\]/i', function( $matches ) {
+         $shuffle = explode('|', $matches[1]);
+         return $shuffle[array_rand($shuffle)];
+      }, $selected_os);
+   }
+   
+   public function processAndroidVersion( $selected_os ) {
+      $this->androidVersion = $version = $this->androidVersions[array_rand($this->androidVersions)];
+      return preg_replace_callback('/:androidVersion:/i', function( $matches ) use ( $version ) { return $version; }, $selected_os);
+   }
+   
+   public function addAndroidDevice( $selected_os ) {
+      $devices = $this->androidDevices[substr($this->androidVersion, 0, 3)];
+      $device  = $devices[array_rand($devices)];
+      
+      $device = $this->processSpinSyntax($device);
+      return preg_replace_callback('/:androidDevice:/i', function( $matches ) use ( $device ) { return $device; }, $selected_os);
    }
    
    public static function chromeVersion( $version ) {
@@ -49,14 +203,15 @@ class userAgent {
    }
    
    /**
-    * Call userAgent::generate([$userAgent = NULL]) to have a UA returned.
+    * Call (new userAgent)->generate('chrome/firefox/mobile') to have a UA returned.
     *    Supported Variables for call: firefox, chrome, mobile, and null is equal to all
     */
    public function generate( $userAgent = NULL ) {
-      if( is_null($userAgent) ) $userAgent = array_rand([ 'firefox' => 1, 'chrome' => 1, 'mobile' => 1 ]);
-      $chrome_version  = [ 'min' => 47, 'max' => 55 ];
-      $firefox_version = [ 'min' => 45, 'max' => 51 ];
+      if( is_null($userAgent) ) $userAgent = array_rand(['firefox' => 1,'chrome' => 1,'mobile' => 1]);
+      $chrome_version  = ['min' => 47,'max' => 55 ];
+      $firefox_version = [ 'min' => 45,'max' => 51];
       //random version from $var['min'] to $var['max']
+      
       
       $chrome_agent  = 'Mozilla/5.0 (:os) AppleWebKit/' . (random_int(1, 100) > 50 ? random_int(533, 537) : random_int(600, 603)) . '.' . random_int(1, 50) . ' (KHTML, like Gecko) Chrome/:version Safari/' . (random_int(1, 100) > 50 ? random_int(533, 537) : random_int(600, 603));
       $firefox_agent = 'Mozilla/5.0 (:os) Gecko/' . (random_int(1, 100) > 30 ? '20100101' : '20130401') . ' Firefox/:version';
@@ -66,11 +221,20 @@ class userAgent {
       
       if( random_int(1, 100) > 50 ) $os .= '; en-US';
       if( $userAgent == 'chrome' ) {
-         return strtr($chrome_agent, [ ':os' => $os, ':version' => self::chromeVersion($chrome_version) ]);
+         return strtr($chrome_agent, [
+           ':os' => $os,
+           ':version' => self::chromeVersion($chrome_version)
+         ]);
       } elseif( $userAgent == 'firefox' ) {
-         return strtr($firefox_agent, [ ':os' => $os, ':version' => self::firefoxVersion($firefox_version) ]);
+         return strtr($firefox_agent, [
+           ':os' => $os,
+           ':version' => self::firefoxVersion($firefox_version)
+         ]);
       } elseif( $userAgent == 'mobile' ) {
-         return strtr($mobile_agent, [ ':os' => $this->mobile_os[random_int(0, count($this->mobile_os) - 1)], ':version' => self::chromeVersion($chrome_version) ]);
+         return strtr($mobile_agent, [
+           ':os' => $this->getMobileOS(),
+           ':version' => self::chromeVersion($chrome_version)
+         ]);
       } else {
          new Exception('Unable to determine user agent to generate');
       }
